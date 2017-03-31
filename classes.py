5647f49da0c2c3bf0649094e34b2dc803bba9631
Timothee <timothee.aupetit@gmail.com>
2017-03-31 22:09:17 +0000

    translated classes.py

--- classes.py
+++ classes.py
@@ -5,130 +5,132 @@ Created on Mon Jul  4 16:29:33 2016
 @author: timotheeaupetit
 """
 
-class CMagasin():
-    taille_caisse = 3   # taille d'une file d'attente à une caisse
-    temps_magasin = 5   # constante : temps qu'un client passe en magasin
-    temps_caisse = 4    # constante : temps qu'un client passe en caisse
-    nb_clients = 0      # nombre de clients
+class Supermarket():
+    checkout_size = 3       # max size of a queue at a checkout
+    time_in_store = 5       # time a customer spends in the store
+    time_at_checkout = 4    # time a customer spends to pay at the checkout
         
     def __init__(self):
-        self.caisses_utilisees = []
+        self.busy_Checkout = []
+        self.customer_count = 0      # amount of customers in the store
         
-    def client_entre(self, d):
-        self.nb_clients += 1
-        print (self.nb_clients, 'client(s) en magasin')
-        evt1 = CEvenement(d + self.calcul_delta(d), 1) #évènement prochain client
-        evt2 = CEvenement(d + self.temps_magasin, 2) #évènement passage en caisse du client qui vient de rentrer
+    def customer_Enter(self, d):
+        self.customer_count += 1
+        print (self.customer_count, 'customer(s) are shopping')
+        evt1 = Event(d + self.timeToNextCustomer(d), 1) # 'next customer' event
+        evt2 = Event(d + Supermarket.time_in_store, 2) # 'to queue' event, for current customer
 
-    def client_attend(self, d):
-        self.nb_clients -= 1
-        if len(self.caisses_utilisees) == 0:
-            self.ouvrir_caisse()
-            print(len(self.caisses_utilisees), 'caisse(s) ouverte(s)')
-        caisse = self.choisir_caisse()
-        taille_file = self.caisses_utilisees[caisse]
-        if taille_file == self.taille_caisse:
-            self.ouvrir_caisse()
-            caisse = self.choisir_caisse()
-        self.caisses_utilisees[caisse] += 1
-        print (self.get_nb_in_caisses(), 'client(s) en caisse')
+    def customer_Queue(self, d):
+        self.customer_count -= 1
+        if len(self.busy_Checkout) == 0:
+            self.openCheckout()
+            print(len(self.busy_Checkout), 'open checkout(s)')
+        caisse = self.chooseCheckout()
+        queueSize = self.busy_Checkout[caisse]
+        if queueSize == Supermarket.checkout_size:
+            self.openCheckout()
+            caisse = self.chooseCheckout()
+        self.busy_Checkout[caisse] += 1
+        print (self.getQueueSize(caisse), 'customer(s) are queuing')
         
-        taille_file = self.caisses_utilisees[caisse]
-        evt = CEvenement(d + 5 * taille_file, 3, caisse)
+        queueSize = self.busy_Checkout[caisse]
+        evt = Event(d + 5 * taille_file, 3, caisse) # 'checkout' event, for current customer
     
-    def client_paye(self, d, c):   
-        evt = CEvenement(d + self.temps_caisse, 4, c) #évènement sortir de la caisse
+    def client_Checkout(self, d, c):   
+        evt = Event(d + Supermarket.time_at_checkout, 4, c) # 'exit' event, for current customer
         
-    def client_sort(self, c):
-        self.caisses_utilisees[c] -= 1
-        if self.caisses_utilisees[c] == 0:
-            self.fermer_caisse(c)
+    def customer_Exit(self, c):
+        self.busy_Checkout[c] -= 1
+        if self.busy_Checkout[c] == 0:
+            self.closeCheckout(c)
         
-    def gerer_magasin(self, Tnow):
-        event = CEvenement(Tnow, 1)
-        while Tnow < 20: #len(event.liste_evt) > 0 #Tant qu'il y a des evenements à traiter
-            print("Liste d'evenements :", event.liste_evt)
+    def manage(self, Tnow):
+        event = Event(Tnow, 1)
+        while Tnow < 20: #len(event.liste_evt) > 0: #Tant qu'il y a des evenements à traiter
+            print("Events:", event.liste_evt)
             print ('Date =', Tnow)
             for evt in event.liste_evt:
                 type_evt = evt[1]
                 caisse = evt[2]
                 if evt[0] == Tnow:
                     if type_evt == 1: #arrivee client en magasin
-                        self.client_entre(Tnow)
+                        self.customer_Enter(Tnow)
                         
                     if type_evt == 2: #arrivee client en file de caisse
-                        self.client_attend(Tnow)          
+                        self.customer_Queue(Tnow)          
         
                     if type_evt == 3: #passage en caisse
-                        self.client_paye(Tnow, caisse)
+                        self.client_Checkout(Tnow, caisse)
         
                     if type_evt == 4: #sortie definitive
-                        self.client_sort(caisse)
+                        self.customer_Exit(caisse)
                         
                     event.sup_evt(Tnow, type_evt, caisse) #supprimer evenement courant
             
             Tnow += 1
-            input ('Entree pour continuer')
+            input ('Press Enter to continue')
 
-    def calcul_delta(self, Tnow):
-        if 0 <= Tnow and Tnow < 60: #Tnow compris entre 9h et 10h
+    def timeToNextCustomer(self, Tnow):
+        if 0 <= Tnow and Tnow < 60:     #Tnow between 9-10am
             return 4
-        if 60 <= Tnow and Tnow < 120: #Tnow compris entre 10h et 11h
+        if 60 <= Tnow and Tnow < 120:   #Tnow between 10-11am
             return 3
-        if 120 <= Tnow and Tnow < 300: #Tnow compris entre 11h et 14h
+        if 120 <= Tnow and Tnow < 300:  #Tnow between 11am-2pm
             return 2
-        if 360 <= Tnow and Tnow < 420: #Tnow est compris entre 14h et 15h
+        if 360 <= Tnow and Tnow < 420:  #Tnow between 2-3pm
             return 4
-        if 420 <= Tnow and Tnow < 480: #Tnow est compris entre 15h et 16h
+        if 420 <= Tnow and Tnow < 480:  #Tnow between 3-4pm
             return 3
-        if 480 <= Tnow and Tnow < 540: #Tnow est compris entre 16h et 17h
+        if 480 <= Tnow and Tnow < 540:  #Tnow between 4-5pm
             return 3
-        if 540 <= Tnow and Tnow < 600: #Tnow est compris entre 17h et 18h
+        if 540 <= Tnow and Tnow < 600:  #Tnow between 5-6pm
             return 1
-        if 600 <= Tnow and Tnow < 645: #Tnow est compris entre 18h et 18h44
+        if 600 <= Tnow and Tnow < 645:  #Tnow between 6-6:44pm
             return 2
 
-    def get_nb_in_caisses(self):
-        nb = 0
-        for caisse in self.caisses_utilisees:
-            nb += self.get_nb_in_caisse(caisse)
-        return nb
+    def getQueueSizes(self):
+        return sum(int(i) for i in self.busy_Checkout)
         
-    def get_nb_in_caisse(self, caisse):
+    def getQueueSize(self, caisse):
         return int(caisse)
         
-    def choisir_caisse(self):
+    def chooseCheckout(self):
         self.index = 0
-        self.plus_courte = self.taille_caisse
+        shortest = Supermarket.checkout_size
         
-        for caisse in self.caisses_utilisees:
-            if caisse < self.plus_courte:
-                self.plus_courte = caisse
-                return self.index               
+        for caisse in self.busy_Checkout:
+            if caisse < shortest:
+                shortest = caisse
+                return self.index            
             self.index +=1
-            
-    def ouvrir_caisse(self):
-        self.caisses_utilisees.append(0)
+    
+    def openCheckout(self):
+        self.busy_Checkout.append(0)
         
-    def fermer_caisse(self, c):
-        self.caisses_utilisees.remove(c)
+    def closeCheckout(self, c):
+        self.caisses_utilissees.remove(c)
 
 #-----------------------------------------------------------------
         
-class CEvenement():
+class Event():
     liste_evt = []
     
     def __init__(self, d, t, c = None):
-        self.liste_evt.append([d, t, c])
-        self.liste_evt.sort()
+        """ Arguments are used as follows:
+        - d: date of the event
+        - t: type of event (1: Enter, 2: Queue, 3: Checkout or 4: Exit)
+        - c: checkout number
+        """
+        Event.liste_evt.append([d, t, c])
+        Event.liste_evt.sort()
        
 #Supprime un evenement de la liste d'evenements
     def sup_evt(self, d, t, c):
-        self.liste_evt.remove([d, t, c])
+        Event.liste_evt.remove([d, t, c])
 
     def get_cur_evt(self, d):
         self.cur_evt = []
-        for evt in self.liste_evt:
+        for evt in Event.liste_evt:
             if evt[0] == d:
                 self.cur_evt.append(evt)
         return self.cur_evt

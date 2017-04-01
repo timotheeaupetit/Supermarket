d09b2e7ab68bdba4331af4edcdd5fd1fee3488ca
Timothée <timothee.aupetit@gmail.com>
2017-04-02 00:51:30 +0200

    added Checkout class + now Event derive from list

--- classes.py
+++ classes.py
@@ -6,70 +6,88 @@ Created on Mon Jul  4 16:29:33 2016
 """
 
 class Supermarket():
-    checkout_size = 3       # max size of a queue at a checkout
     time_in_store = 5       # time a customer spends in the store
     time_at_checkout = 4    # time a customer spends to pay at the checkout
         
     def __init__(self):
-        self.busy_Checkout = []
-        self.customer_count = 0      # amount of customers in the store
-        
-    def customer_Enter(self, d):
-        self.customer_count += 1
-        print (self.customer_count, 'customer(s) are shopping')
-        evt1 = Event(d + self.timeToNextCustomer(d), 1) # 'next customer' event
-        evt2 = Event(d + Supermarket.time_in_store, 2) # 'to queue' event, for current customer
-
-    def customer_Queue(self, d):
-        self.customer_count -= 1
-        if len(self.busy_Checkout) == 0:
-            self.openCheckout()
-            print(len(self.busy_Checkout), 'open checkout(s)')
-        caisse = self.chooseCheckout()
-        queueSize = self.busy_Checkout[caisse]
-        if queueSize == Supermarket.checkout_size:
-            self.openCheckout()
-            caisse = self.chooseCheckout()
-        self.busy_Checkout[caisse] += 1
-        print (self.getQueueSize(caisse), 'customer(s) are queuing')
-        
-        queueSize = self.busy_Checkout[caisse]
-        evt = Event(d + 5 * queueSize, 3, caisse) # 'checkout' event, for current customer
-    
-    def client_Checkout(self, d, c):   
-        evt = Event(d + Supermarket.time_at_checkout, 4, c) # 'exit' event, for current customer
-        
-    def customer_Exit(self, c):
-        self.busy_Checkout[c] -= 1
-        if self.busy_Checkout[c] == 0:
-            self.closeCheckout(c)
+        self.Events = []            # list of ordered events
+        self.busyCheckouts = []     # list containing open checkouts
+        self.customerCount = 0      # amount of customers in the store
         
     def manageSupermarket(self, Tnow):
-        event = Event(Tnow, 1)
-        while Tnow < 20: #len(event.liste_evt) > 0: #Tant qu'il y a des evenements à traiter
-            print("Events:", event.liste_evt)
+        self.Events(Event(Tnow, 1))
+        
+        while Tnow < 20: #len(self.Events) > 0: # While remaining events
+            print("Events:", self.Events)
             print ('Date =', Tnow)
-            for evt in event.liste_evt:
+            
+            for evt in self.Events:
                 type_evt = evt[1]
-                caisse = evt[2]
+                checkout = evt[2]
+                
                 if evt[0] == Tnow:
-                    if type_evt == 1: #arrivee client en magasin
+                    if type_evt == 1: # customer go in
                         self.customer_Enter(Tnow)
                         
-                    if type_evt == 2: #arrivee client en file de caisse
+                    if type_evt == 2: # customer queue before checkout
                         self.customer_Queue(Tnow)          
         
-                    if type_evt == 3: #passage en caisse
-                        self.client_Checkout(Tnow, caisse)
+                    if type_evt == 3: # customer pay
+                        self.customer_Checkout(Tnow, checkout)
         
-                    if type_evt == 4: #sortie definitive
-                        self.customer_Exit(caisse)
+                    if type_evt == 4: # customer go out
+                        self.customer_Exit(checkout)
                         
-                    event.sup_evt(Tnow, type_evt, caisse) #supprimer evenement courant
+                    self.Events.remove([Tnow, type_evt, checkout]) #supprimer evenement courant
             
-            Tnow += 1
+            Tnow += 1  
+            
+    def customer_Enter(self, d):
+        """
+        This method adds 1 to the customer count, then creates 2 events: 
+            - the next customer's arrival
+            - the current customer's move to checkout's buffer
+        """
+        self.customerCount += 1
+        print (self.customerCount, 'customer(s) are shopping')
+        
+        self.Events.append(Event(d + self.timeToNextCustomer(d), 1)) # 'next customer' event
+        self.Events.append(Event(d + Supermarket.time_in_store, 2)) # 'to queue' event, for current customer
+        self.Events.sort()
 
+    def customer_Queue(self, d):
+        """
+        This method remove 1 to the customer count...
+        """
+        self.customerCount -= 1
+        
+        if len(self.busyCheckouts) == 0:
+            self.openCheckout()
+            print(len(self.busyCheckouts), 'open checkout(s)')
+            
+        chk = self.chooseCheckout()
+        
+        if chk.queueSize == Checkout.queueCapacity:
+            chk = self.openCheckout()
+            
+        chk.addToQueue()
+        print (chk.queueSize, 'customer(s) are queuing')
+        
+        self.Events.append(Event(d + Checkout.checkoutDuration * chk.queueSize, 3, chk)) # 'checkout' event, for current customer
+    
+    def customer_Checkout(self, d, checkout):   
+        checkout.removeFromQueue()
+        self.Events.append(Event(d + Supermarket.time_at_checkout, 4, checkout)) # 'exit' event, for current customer
+        
+    def customer_Exit(self, checkout):
+        
+        if checkout.queueSize == 0:
+            self.closeCheckout(checkout)
+        
     def timeToNextCustomer(self, Tnow):
+        """
+        Calculate the time between each customer arrival, based on the hour range
+        """
         if 0 <= Tnow and Tnow < 60:     #Tnow between 9-10am
             return 4
         if 60 <= Tnow and Tnow < 120:   #Tnow between 10-11am
@@ -88,44 +106,31 @@ class Supermarket():
             return 2
 
     def getQueueSizes(self):
-        return sum(int(i) for i in self.busy_Checkout)
-        
-    def getQueueSize(self, caisse):
-        return int(caisse)
+        """
+        Returns the total amount of customers queuing before all checkouts
+        """
+        return sum(i.queueSize for i in self.busyCheckouts)
         
     def chooseCheckout(self):
-        self.index = 0
-        shortest = Supermarket.checkout_size
+        index = 0
+        shortest = min(i.queueSize for i in self.busyCheckouts)
         
-        for caisse in self.busy_Checkout:
-            if caisse < shortest:
-                shortest = caisse
-                return self.index            
-            self.index +=1
+        for checkout in self.busyCheckouts:
+            if checkout.queueSize == shortest:
+                return self.busyCheckouts[index]          
+            index += 1
     
     def openCheckout(self):
-        self.busy_Checkout.append(0)
+        chk = Checkout()
+        self.busyCheckouts.append(chk)
+        return chk
         
-    def closeCheckout(self, c):
-        self.caisses_utilissees.remove(c)
-
-#-----------------------------------------------------------------
+    def closeCheckout(self, checkout):
+        self.busyCheckouts.remove(checkout)
         
-class Event():
-    liste_evt = []
-    
-    def __init__(self, d, t, c = None):
-        """ Arguments are used as follows:
-        - d: date of the event
-        - t: type of event (1: Enter, 2: Queue, 3: Checkout or 4: Exit)
-        - c: checkout number
-        """
-        Event.liste_evt.append([d, t, c])
-        Event.liste_evt.sort()
-       
 #Supprime un evenement de la liste d'evenements
     def sup_evt(self, d, t, c):
-        Event.liste_evt.remove([d, t, c])
+        self.Events.remove([d, t, c])
 
     def get_cur_evt(self, d):
         self.cur_evt = []
@@ -133,3 +138,27 @@ class Event():
             if evt[0] == d:
                 self.cur_evt.append(evt)
         return self.cur_evt
+    
+#-----------------------------------------------------------------
+class Checkout():        
+    queueCapacity = 3       # max size of a queue at a checkout
+    checkoutDuration = 4    # time a customer spends to pay at the checkout
+    
+    def __init__(self):
+        self.queueSize = 0      # size of a queue at a checkout
+    
+    def addToQueue(self):
+        self.queueSize += 1
+    
+    def removeFromQueue(self):
+        self.queueSize -= 1
+#-----------------------------------------------------------------    
+class Event(list):
+    
+    def __init__(self, d, t, c = None):
+        """ Arguments are used as follows:
+        - d: date of the event
+        - t: type of event (1: Enter, 2: Queue, 3: Checkout or 4: Exit)
+        - c: checkout number
+        """
+        self.append(d, t, c)
--- main.py
+++ main.py
@@ -8,8 +8,8 @@ Created on Mon Jul  4 16:29:33 2016
 from classes import Supermarket
 
 def main():
-    supermarket = Supermarket()
-    Tnow = 0
+    supermarket = Supermarket() # we only need to create one supermarket
+    Tnow = 0                    # time arbitrarily starts at 0
     supermarket.manageSupermarket(Tnow)
     
 if __name__ == '__main__':

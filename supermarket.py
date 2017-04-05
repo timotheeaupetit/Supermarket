6a88c68926f00ae7a10ac187cdb4bd4d23502644
Timothée <timothee.aupetit@gmail.com>
2017-04-05 23:13:48 +0200

    renamed classes.py to supermarket.py + added call to graphs.py

--- supermarket.py
+++ supermarket.py
@@ -5,6 +5,7 @@ Created on Mon Jul  4 16:29:33 2016
 
 @author: timotheeaupetit
 """
+from graphs import RushHour
 
 class Supermarket(object):
     
@@ -14,19 +15,23 @@ class Supermarket(object):
     def __init__(self):
         self.Events = []            # list of events
         self.busyCheckouts = []     # list containing open checkouts
-        self.customerCount = 0      # amount of customers in the store
+        self.customerInStore = 0    # amount of customers in the store
+        self.customerShopping = 0   # amount of customers shopping    
+        self.time = []
+        self.customerCount = []
+        self.checkoutCount = []
         
     def manageSupermarket(self, Tnow):
         self.Events.append(Event(Tnow, 1)) # initial Event
         
-        while Tnow < 120: #len(self.Events) > 0: # While remaining events
+        while len(self.Events) > 0: # While remaining events
             self.Events.sort(key=lambda x: x[0]) # reorder events by their dates
             
-            print ('\nDate =', Tnow)    
-            print("Events:", self.Events)
-            print(self.customerCount, 'customer(s) are shopping')
-            print(self.getQueueSizes(), 'customer(s) are queuing')
-            print(len(self.busyCheckouts), 'open checkout(s)')
+#            print ('\nDate =', Tnow)    
+#            print("Events:", self.Events)
+#            print(self.customerShopping, 'customer(s) are shopping')
+#            print(self.getQueueSizes(), 'customer(s) are queuing')
+#            print(len(self.busyCheckouts), 'open checkout(s)')
             
             oldEvents = []
             
@@ -52,8 +57,15 @@ class Supermarket(object):
             
             self.Events = [evt for evt in self.Events if evt not in oldEvents]
             
+            self.time.append(Tnow)
+            self.customerCount.append(self.customerInStore)
+            self.checkoutCount.append(len(self.busyCheckouts))
+            
             Tnow += 1  
             #input('Press enter to continue')
+            
+        statistics = RushHour(self.time, self.customerCount, self.checkoutCount)
+        statistics.graphe1()
             
     def customer_Enter(self, d):
         """
@@ -61,16 +73,18 @@ class Supermarket(object):
             - the next customer's arrival
             - the current customer's move to checkout's buffer
         """
-        self.customerCount += 1
+        self.customerInStore += 1
+        self.customerShopping += 1
         
-        self.Events.append(Event(d + Supermarket.timeToNextCustomer(d), 1)) # 'next customer' event
-        self.Events.append(Event(d + Supermarket.time_in_store, 2)) # 'to queue' event, for current customer
+        if d < 645:
+            self.Events.append(Event(d + Supermarket.timeToNextCustomer(d), 1)) # 'next customer' event
+            self.Events.append(Event(d + Supermarket.time_in_store, 2)) # 'to queue' event, for current customer
 
     def customer_Queue(self, d):
         """
         Defines what happens when a customer has finished shopping and reaches the queues
         """
-        self.customerCount -= 1
+        self.customerShopping -= 1
         
         if len(self.busyCheckouts) == 0:
             chk = self.openCheckout()
@@ -81,20 +95,23 @@ class Supermarket(object):
             
         chk.addToQueue()
         
-        self.Events.append(Event(d + Checkout.checkoutDuration * chk.queueSize, 3, chk)) # 'checkout' event, for current customer
+        if d < 645:
+            self.Events.append(Event(d + Checkout.checkoutDuration * chk.queueSize, 3, chk)) # 'checkout' event, for current customer
     
     def customer_Checkout(self, d, chk):
         """
         Defines what happens when a customer exits the queue and reaches the checkout
         """
         chk.removeFromQueue() # remove one customer from the chk queue
-        self.Events.append(Event(d + Supermarket.time_at_checkout, 4, chk)) # 'exit' event, for current customer
+        if d < 645:
+            self.Events.append(Event(d + Supermarket.time_at_checkout, 4, chk)) # 'exit' event, for current customer
         
     def customer_Exit(self, chk):
         """
         Defines what happens when a customer exits the checkout (and therefore the supermarket)
         """
-        print('1 customer exit from checkout', chk.id, '\n')
+        #print('1 customer exit from checkout', chk.id, '\n')
+        self.customerInStore -= 1
         if chk.queueSize == 0:
             self.closeCheckout(chk)
                 
@@ -130,27 +147,29 @@ class Supermarket(object):
         Returns the total amount of customers queuing before all checkouts
         """
         return sum(chk.queueSize for chk in self.busyCheckouts)
-        
+
     def timeToNextCustomer(Tnow):
         """
         Calculates the time between each customer arrival, based on the hour range
         """
-        if 0 <= Tnow and Tnow < 60:     #Tnow between 9-10am
+        if Tnow < 60:     #Tnow between 9-10am
             return 4
-        if 60 <= Tnow and Tnow < 120:   #Tnow between 10-11am
+        elif Tnow < 120:  #Tnow between 10-11am
             return 3
-        if 120 <= Tnow and Tnow < 300:  #Tnow between 11am-2pm
+        elif Tnow < 300:  #Tnow between 11am-2pm
             return 2
-        if 360 <= Tnow and Tnow < 420:  #Tnow between 2-3pm
+        elif Tnow < 420:  #Tnow between 2-3pm
             return 4
-        if 420 <= Tnow and Tnow < 480:  #Tnow between 3-4pm
+        elif Tnow < 480:  #Tnow between 3-4pm
             return 3
-        if 480 <= Tnow and Tnow < 540:  #Tnow between 4-5pm
+        elif Tnow < 540:  #Tnow between 4-5pm
             return 3
-        if 540 <= Tnow and Tnow < 600:  #Tnow between 5-6pm
+        elif Tnow < 600:  #Tnow between 5-6pm
             return 1
-        if 600 <= Tnow and Tnow < 645:  #Tnow between 6-6:44pm
+        elif Tnow < 645:  #Tnow between 6-6:44pm
             return 2
+        else: 
+            return None
 
 #-----------------------------------------------------------------
 class Checkout(object):        

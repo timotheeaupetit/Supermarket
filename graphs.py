02f6174830fa778e190f3cd558fc3f4f6f661d4f
Timothée <timothee.aupetit@gmail.com>
2017-04-05 21:54:01 +0200

    added new class file to handle data visualization

---
+++ graphs.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  5 21:46:42 2017
+
+@author: timotheeaupetit
+"""
+
+class RushHour:
+    def graphe1(self):
+        return self
